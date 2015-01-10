db821b99525e5259fca4a7bc53dc7302ab19afcd
Scot Clausing <tsclausing@gmail.com>
2015-01-09 21:30:33 -0600

    Simplify the lexer

--- src/compiler/lexer.py
+++ src/compiler/lexer.py
@@ -8,24 +8,18 @@ from compiler.exceptions import LexException
 Numeral = namedtuple('Numeral', 'pos value')
 
 
-def lex(string) -> [Numeral]:
+def lex(source) -> [Numeral]:
     """
     >>> list(lex("XLII"))
     [Numeral(pos=0, value='XL'), Numeral(pos=2, value='I'), Numeral(pos=3, value='I')]
     """
+    values = ("IV", "IX", "XL", "XC", "CD", "CM", "I", "V", "X", "L", "C", "D", "M")
+
     pos = 0
-    while string:
-        token, string, pos = pop_token(string, pos)
-        yield token
-
-
-def pop_token(string, pos=0):
-    """
-    >>> pop_token("XLII", 0)
-    (Numeral(pos=0, value='XL'), 'II', 2)
-    """
-    for pattern in ("IV", "IX", "XL", "XC", "CD", "CM", "I", "V", "X", "L", "C", "D", "M"):
-        if string.startswith(pattern):
-            return Numeral(pos, pattern), string[len(pattern):], pos+len(pattern)
-
-    raise LexException("Lex error: unknown character '%s' at index %d" % (string[0], pos))
+    while pos < len(source):
+        value = next((value for value in values if source.startswith(value, pos)), None)
+        if value:
+            yield Numeral(pos, value)
+            pos += len(value)
+        else:
+            raise LexException("Lex error: unknown character '%s' at index %d" % (source[pos], pos))
